46bb88e4f99c562c3089652206a9912ec763461a
Alexander.Germanov <Alexander.Germanov@russianpost.ru>
2024-09-11 10:44:14 +0300

    добавил comments и отформатировал

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,20 @@
 create table if not exists users(
-                                    id BIGINT generated always as identity primary key,
-                                    name varchar(100),
+    id bigint generated always as identity primary key,
+    "name" varchar(100),
     email varchar(320),
-    CONSTRAINT UQ_USER_EMAIL UNIQUE (email)
+    constraint uq_user_email unique (email)
     );
 
 create table if not exists requests(
-                                       id BIGINT generated always as identity primary key,
-                                       description varchar(1000),
+    id bigint generated always as identity primary key,
+    description varchar(1000),
     requestor_id bigint,
     constraint fk_requests_to_users foreign key(requestor_id) references users(id)
     );
 
 create table if not exists items(
-                                    id BIGINT generated always as identity primary key,
-                                    name varchar(100),
+    id bigint generated always as identity primary key,
+    "name" varchar(100),
     description varchar(1000),
     is_available bool,
     owner_id bigint,
@@ -24,12 +24,22 @@ create table if not exists items(
     );
 
 create table if not exists bookings(
-                                       id BIGINT generated always as identity primary key,
-                                       start_date timestamp without time zone,
-                                       end_date timestamp without time zone,
-                                       item_id bigint,
-                                       booker_id bigint,
-                                       status varchar(50),
+    id bigint generated always as identity primary key,
+    start_date timestamp without time zone,
+    end_date timestamp without time zone,
+    item_id bigint,
+    booker_id bigint,
+    status varchar(50),
     constraint fk_bookings_to_items foreign key(item_id) references items(id),
     constraint fk_bookings_to__users foreign key(booker_id) references users(id)
+    );
+
+create table if not exists "comments" (
+    id bigint generated always as identity primary key,
+    "text" varchar(1000) null,
+    item_id int8 null,
+    author_id int8 null,
+    created timestamp null,
+    constraint fk_comments_to_items foreign key (item_id) references public.items(id),
+    constraint fk_comments_to_users foreign key (author_id) references public.users(id)
     );
